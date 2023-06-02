f43e30ba2f22aac489ceafda2d852acbbfdbab73
Kunal Vijay <kunalvijay.kv2002@gmail.com>
2023-06-02 16:04:45 +0530

    None

---
+++ index.js
@@ -0,0 +1,115 @@
+require('dotenv').config()
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")  //mechanism by which a front-end client can make requests for resources to an external back-end server
+
+//APP config
+const app = express() //Object of express
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+
+//DB config
+// mongoose.connect('mongodb://localhost:27017/reminderAppDB', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }, () => console.log("DB connected"))
+mongoose.connect("mongodb+srv://" + process.env.USERNAMEE + ":" + process.env.PASSWORD + "@cluster0.0mx8qku.mongodb.net/reminderAppDB"
+    , { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log("DB connected"));
+const reminderSchema = new mongoose.Schema({
+    reminderMsg: String,
+    remindAt: String,
+    isReminded: Boolean  //To confirm whether reminder has been reminded or not
+})
+const Reminder = new mongoose.model("reminder", reminderSchema)
+
+
+//Whatsapp reminding functionality
+
+setInterval(() => {
+    Reminder.find({}, (err, reminderList) => {
+        if (err) {
+            console.log(err)
+        }
+        if (reminderList) {
+            reminderList.forEach(reminder => {
+                if (!reminder.isReminded) {
+                    const now = new Date()
+                    if ((new Date(reminder.remindAt) - now) < 0) {  //It is a past time or current time so trigger alert
+                        Reminder.findByIdAndUpdate(reminder._id, { isReminded: true }, (err, remindObj) => {
+                            if (err) {
+                                console.log(err)
+                            }
+                            const accountSid = process.env.ACCOUNT_SID
+                            const authToken = process.env.AUTH_TOKEN
+                            // const accountSid = 'AC3587a1135e5f5156434a97a408072d22'
+                            // const authToken = '6f8a50c387aa349d3637e49d4802bcdd'
+                            const client = require('twilio')(accountSid, authToken);
+                            client.messages
+                                .create({
+                                    body: reminder.reminderMsg,
+                                    from: 'whatsapp:+14155238886',
+                                    to: 'whatsapp:+918178050765' //YOUR PHONE NUMBER INSTEAD OF 8888888888
+                                })
+                                .then(message => console.log(message.sid))
+                                .done()
+                        })
+                    }
+                }
+            })
+        }
+    })
+}, 1000)
+    ;
+
+
+//API routes
+app.get("/getAllReminder", (req, res) => {
+    Reminder.find({}, (err, reminderList) => {
+        if (err) {
+            console.log(err)
+        }
+        if (reminderList) {
+            res.send(reminderList)
+        }
+    })
+})
+app.post("/addReminder", (req, res) => {
+    const { reminderMsg, remindAt } = req.body
+    const reminder = new Reminder({
+        reminderMsg,
+        remindAt,
+        isReminded: false
+    })
+    reminder.save(err => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.redirect("/getAllReminder");
+        }
+        // Reminder.find({}, (err, reminderList) => {
+        //     if (err) {
+        //         console.log(err)
+        //     }
+        //     if (reminderList) {
+        //         res.send(reminderList)
+        //     }
+        // })
+    })
+
+})
+app.post("/deleteReminder", (req, res) => {
+    Reminder.deleteOne({ _id: req.body.id }, () => { //Deleting based on ID
+        Reminder.find({}, (err, reminderList) => {
+            if (err) {
+                console.log(err)
+            }
+            if (reminderList) {
+                res.send(reminderList)
+            }
+        })
+    })
+})
+
+app.listen(9000, () => console.log("Be started"))
---
+++ reminder-app-frontend/src/App.js
@@ -0,0 +1,82 @@
+import './App.css'
+import React, { useState, useEffect } from "react"
+import axios from "axios"  //For making API calls to third party to fetch data
+import DateTimePicker from "react-datetime-picker"
+// import DeleteIcon from "@material-ui/icons/Delete";
+import DeleteIcon from "@material-ui/icons/Delete";
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import TodayIcon from '@mui/icons-material/Today';
+import AddIcon from '@mui/icons-material/Add';
+
+function App() {
+
+  const [reminderMsg, setReminderMsg] = useState("")
+  const [remindAt, setRemindAt] = useState()
+  const [reminderList, setReminderList] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:9000/getAllReminder").then(res => setReminderList(res.data)) //To perform effect after a component is rendered
+  }, [])
+
+  const addReminder = () => {
+    axios.post("http://localhost:9000/addReminder", { reminderMsg, remindAt })
+      .then(res => setReminderList(res.data))
+    setReminderMsg("")
+    setRemindAt()
+  }
+
+  const deleteReminder = (id) => {
+    axios.post("http://localhost:9000/deleteReminder", { id })
+      .then(res => setReminderList(res.data))
+  }
+
+  return (
+    <div className="App">
+      <div className="homepage">
+        <header>
+          <h1>
+            <NotificationsActiveIcon />
+            WhatsAlert
+          </h1>
+        </header>
+        <div className="homepage_header">
+          <h1>Remind Me <TodayIcon className='today' sx={{ fontSize: 30 }} /></h1>
+          <input type="text" className="reminder" placeholder=" Add Reminder notes here..." value={reminderMsg} onChange={e => setReminderMsg(e.target.value)} />
+          <DateTimePicker className="reminder"
+            value={remindAt}
+            onChange={setRemindAt}
+            minDate={new Date()}
+            minutePlaceholder="mm"
+            hourPlaceholder="hh"
+            dayPlaceholder="DD"
+            monthPlaceholder="MM"
+            yearPlaceholder="YYYY"
+          />
+          <div className="button" onClick={addReminder}>{/*<AddIcon/>*/}<span>Add Reminder</span></div>
+        </div>
+
+
+        <div className="homepage_body">
+          {
+            reminderList.map(reminder => (
+              <div className="reminder_card" key={reminder._id}>
+                <h2>Message: <span class="ctext">{reminder.reminderMsg}</span></h2>
+                <h3>Reminder Set at:</h3>
+                <p>{String(new Date(reminder.remindAt.toLocaleString(undefined, { timezone: "Asia/Kolkata" })))}</p> {/*Setting date as per our time zone */}
+                {/* <div className="button" onClick={() => deleteReminder(reminder._id)}>Delete</div> */}
+                <button className='del' onClick={() => deleteReminder(reminder._id)}>
+                  <DeleteIcon />
+                </button>
+              </div>
+            ))
+          }
+
+
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App;
