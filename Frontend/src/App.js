430965dad211606c21b9080d166d69aa1dbd8636
Kunal Vijay <kunalvijay.kv2002@gmail.com>
2023-06-04 21:35:49 +0530

    Added comments

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -14,9 +14,9 @@ function App() {
   const [remindAt, setRemindAt] = useState()
   const [reminderList, setReminderList] = useState([])
 
-  useEffect(() => {
+  useEffect(() => { //Basically it displays all the reminders previously set when page is loaded.
     axios.get("https://whatsalert.onrender.com/getAllReminder").then(res => setReminderList(res.data)) //To perform effect after a component is rendered
-  }, [])
+  }, []) //If we specify something in box like reminderMsg then whenever thr message gets changed the useEffect does action specified in it.
 
   const addReminder = () => {
     axios.post("https://whatsalert.onrender.com/addReminder", { reminderMsg, remindAt })
@@ -59,12 +59,12 @@ function App() {
         <div className="homepage_body">
           {
             reminderList.map(reminder => (
-              <div className="reminder_card" key={reminder._id}>
+              <div className="reminder_card" key={reminder._id}>  {/* key specified as it helps in mapping*/}
                 <h2>Message: <span class="ctext">{reminder.reminderMsg}</span></h2>
                 <h3>Reminder Set at:</h3>
                 <p>{String(new Date(reminder.remindAt.toLocaleString(undefined, { timezone: "Asia/Kolkata" })))}</p> {/*Setting date as per our time zone */}
                 {/* <div className="button" onClick={() => deleteReminder(reminder._id)}>Delete</div> */}
-                <button className='del' onClick={() => deleteReminder(reminder._id)}>
+                <button className='del' onClick={() => deleteReminder(reminder._id)}> {/* Deleting based on unique id */}
                   <DeleteIcon />
                 </button>
               </div>
